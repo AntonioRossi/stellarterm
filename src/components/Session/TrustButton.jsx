bd7c9e509598bd7cfecfbd0571dfc9da580f2ec4
shiny-mountain <shiny-mountain@lobstr.co>
2019-03-21 12:51:19 +0300

    Fixed display 'unknown' domain for assets in search by anchor. If there is a logo from the server - show it.

--- src/components/Session/TrustButton.jsx
+++ src/components/Session/TrustButton.jsx
@@ -33,6 +33,7 @@ export default class TrustButton extends React.Component {
                 return bssResult.serverResult
                     .then(() => {
                         this.setState({ status: 'ready' });
+                        this.addDataToLocalStorage();
                     })
                     .catch((error) => {
                         let errorType = 'unknown';
@@ -46,6 +47,22 @@ export default class TrustButton extends React.Component {
                         });
                     });
             });
+    }
+
+    addDataToLocalStorage() {
+        const { asset, host, currency, color } = this.props;
+        if (asset.domain === undefined && currency && host) {
+            const unknownAsset = {
+                code: asset.code,
+                issuer: asset.issuer,
+                host,
+                currency,
+                color,
+                time: new Date(),
+            };
+            const unknownAssetsData = JSON.parse(localStorage.getItem('unknownAssetsData')) || [];
+            localStorage.setItem('unknownAssetsData', JSON.stringify([...unknownAssetsData, unknownAsset]));
+        }
     }
 
     checkAssetForAccept() {
@@ -135,4 +152,10 @@ TrustButton.propTypes = {
     message: PropTypes.string.isRequired,
     trustMessage: PropTypes.string.isRequired,
     isManualTrust: PropTypes.bool,
+    host: PropTypes.string,
+    color: PropTypes.string,
+    currency: PropTypes.shape({
+        image: PropTypes.string,
+        host: PropTypes.string,
+    }),
 };
